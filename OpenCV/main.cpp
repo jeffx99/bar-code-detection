e6f7b5a520c5564d628957bc3b7229b39de323b0
Meet Patel <meet.s.patel.nj@gmail.com>
2016-12-07 20:48:48 -0500

    new method

--- OpenCV/main.cpp
+++ OpenCV/main.cpp
@@ -6,10 +6,6 @@
 
 using namespace cv;
 
-int roundToTens(int x) {
-	return ((x + 5) / 10) * 10;
-}
-
 int main() {
 
 	Mat src = imread("C:\\Users\\mpatel\\Documents\\Visual Studio 2015\\Projects\\OpenCV\\data\\samplecode25x.png");
@@ -20,58 +16,54 @@ int main() {
 	// namedWindow("source", CV_WINDOW_NORMAL);
 	// imshow("source", src);
 
-	// Preproccessing
+	// Preprocessing
+	Mat pre;
+	cvtColor(src, pre, CV_BGR2GRAY);
+	//GaussianBlur(pre, pre, Size{ 25, 25 }, 2, 2);
+
+	Mat dst;
+	resize(src, dst, Size{ 250,109 });
+
+	// Resize image
 	Mat gray;
-	cvtColor(src, gray, CV_BGR2GRAY);
-	GaussianBlur(gray, gray, Size(25, 25), 2, 2);
+	resize(pre, gray, Size{ 250,109 });
 
-	// Find the circles in the picture
-	std::vector<Vec3f> circles;
-	HoughCircles(gray, circles, CV_HOUGH_GRADIENT, 1, 30, 200, 35, 0, 100);
-	for (auto const &data : circles) {
-		Point center(cvRound(data[0]), cvRound(data[1]));
-		int radius = cvRound(data[2]);
-	}
+	// namedWindow("Reduced", CV_WINDOW_NORMAL);
+	// imshow("Reduced", gray);
 
-	// Extract the center points
-	std::vector<Point> centers;
-	for (auto const &circle : circles) {
-		centers.push_back(Point{ cvRound(circle[0]), cvRound(circle[1]) });
-	}
+	// Construct array of possible locations for dots.
+	Point possible_pts[168];
+	// Construct array of alignment points
+	Point alignment_pts[6]{
+		Point(5, 30)/*,
+		Point(39,30),
+		Point(66, 30),
+		Point(160, 30),
+		Point(187, 30),
+		Point(221, 30)*/
+	};
 
-	// Sort the center points based on their X-coordinate
-	std::sort(centers.begin(), centers.end(),
-		[](const Point &a, const Point &b) {
-		return a.x < b.x;
-	});
-	
-	// Round the center points
-	for (auto &center : centers) {
-		center.x = roundToTens(center.x);
-		center.y = roundToTens(center.y);
-	}
-
-	for (auto const &data : centers) {
-		std::cout << data << std::endl;
-	}
-
-	for (size_t i = 0; i < centers.size(); i++) {
-		// std::cout << i << "\tCenter: " << centers[i] << std::endl;
-		if (i != 0) {
-			// std::cout << "\t\tDistance from prior center X: \t" << centers[i].x - centers[i - 1].x << std::endl;
+	// Iterate through each alignment point, and 
+	for (auto alignment_pt : alignment_pts) {
+		int k = 0;
+		Point marker(5, 30);
+		for (int i = 0; i < 4; ++i) {
+			for (int j = 0; j < 7; ++j) {
+				possible_pts[j + i*7 + k*24] = marker;
+				marker.y += 7;
+			}
+			marker.y = 30;
+			marker.x += 7;
 		}
+		++k;
 	}
 
-	// Draw the circles on the rounded position
-	for (auto const &data : centers) {
-		circle(src, data, 10, Scalar(0, 0, 255), 3, 8, 0);
+	for (auto const &data : possible_pts) {
+		circle(dst, data, 0.5, Scalar(0, 255, 0), 3, 8, 0);
 	}
 
-	std::vector<Point> all_possible_centers;
-
-
-	namedWindow("Hough Circles", CV_WINDOW_NORMAL);
-	imshow("Hough Circles", src);
+	namedWindow("final", CV_WINDOW_NORMAL);
+	imshow("final", dst);
 
 	waitKey(0);
 	return 0;
